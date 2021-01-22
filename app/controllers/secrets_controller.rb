408058cf9bfef19bb7b40a6df6d38df03d7c5d33
Jeremy Wood <js.wood206@gmail.com>
2021-01-21 20:40:28 -0800

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,16 +1,15 @@
 class SecretsController < ApplicationController
+    before_action :user_logged_in?
 
     def show
-        not_logged_in?
     end
 
     def secret
-        not_logged_in?
     end
 
     private
 
-    def not_logged_in?
+    def user_logged_in?
         if session[:name].blank?
             redirect_to login_path
         end
